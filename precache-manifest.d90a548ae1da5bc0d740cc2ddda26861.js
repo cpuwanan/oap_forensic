922f2365167abc962e6e8ff9e4ce46bf31e841ac
PUWANAN CHUMTONG <puwanan@PUWANANs-MacBook-Pro.local>
2020-11-20 16:47:02 +0700

    Updates

---
+++ precache-manifest.d90a548ae1da5bc0d740cc2ddda26861.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4eaa6804d62511202418262e21086e4",
+    "url": "/oap_forensic/index.html"
+  },
+  {
+    "revision": "5b6fae3ccd64b14daa83",
+    "url": "/oap_forensic/static/css/2.ccef0026.chunk.css"
+  },
+  {
+    "revision": "bfd54f50ad59152da5cf",
+    "url": "/oap_forensic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5b6fae3ccd64b14daa83",
+    "url": "/oap_forensic/static/js/2.d04db333.chunk.js"
+  },
+  {
+    "revision": "3c45ea21ae39fd33204d7de3baeeaaa3",
+    "url": "/oap_forensic/static/js/2.d04db333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfd54f50ad59152da5cf",
+    "url": "/oap_forensic/static/js/main.e7978e2a.chunk.js"
+  },
+  {
+    "revision": "bb30dc3e0af669fc2a06",
+    "url": "/oap_forensic/static/js/runtime-main.3d5e8992.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/oap_forensic/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/oap_forensic/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/oap_forensic/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/oap_forensic/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/oap_forensic/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/oap_forensic/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/oap_forensic/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/oap_forensic/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/oap_forensic/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/oap_forensic/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/oap_forensic/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/oap_forensic/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/oap_forensic/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/oap_forensic/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/oap_forensic/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/oap_forensic/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/oap_forensic/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-export const NETWORKING = {
-    ip: 'http://localhost',
-    backendPort: 4000
-};
-
-export const KEYS = {
-    websocket: "websocket_status",
-    radioactive_data: "radioactive_data"
-}
